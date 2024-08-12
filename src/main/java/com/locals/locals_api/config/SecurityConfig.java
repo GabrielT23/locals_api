b00ac823907e5f8544f248fd9d33c01ae7ff858b
GabrielT23 <gabaraujoteixeira@gmail.com>
2024-08-12 20:33:02 -0300

    feat:config-privates-and-public-routes

--- src/main/java/com/locals/locals_api/config/SecurityConfig.java
+++ src/main/java/com/locals/locals_api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.locals.locals_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,15 +10,23 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/users").permitAll() 
+                .requestMatchers(HttpMethod.GET, "/locals").permitAll() 
+                .anyRequest().authenticated()
+            );
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEnconder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
+
+
+
