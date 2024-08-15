e55be24dbfb96214b2ae55fd9d459505005c4016
GabrielT23 <gabaraujoteixeira@gmail.com>
2024-08-14 22:19:53 -0300

    feat:config-properties

--- src/test/java/com/locals/locals_api/LocalsApiApplicationTests.java
+++ src/test/java/com/locals/locals_api/LocalsApiApplicationTests.java
@@ -3,11 +3,17 @@ package com.locals.locals_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootTest
 class LocalsApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD")); 
 	}
 
 }
